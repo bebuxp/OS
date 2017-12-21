c48e7eb8be5c4781e37eb71f76572df41bd9f2f7
Ebtehal Aljedaani <33575887+bebuxp@users.noreply.github.com>
2017-12-21 21:13:24 +0300

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -6,5 +6,48 @@ import java.util.LinkedList;
 
 public abstract class Queue  {
 
+  // specify object data and property fields
+	
+     LinkList queue ;  
+
+	
+public Queue()
+    {
+        queue=new LinkList();
+    }
+
+
+
+// -----------------------------------------------------------------------
+// method functions used by Queue() object
+//
+
+// specify (i.e., design) interfaces details (arguments and return values)
+
+// --------------------
+// implement queue empty condition based on linked list .isEmpty() method
+
+public boolean isQueueEmpty()              // note "q" prefix to distinguish from similar 
+                               // function in linked-list package
+{
+	 return Queue.isEmpty();               // (replace by correct call)
+}
+  
+
+// --------------------
+public <T> void enqueue( T item)
+{
+  Queue.insert(item);
+}
+
+// --------------------
+
+  public T dequeue()
+{
+
+   if(!queue.isEmpty())
+            queue.deleteFirst();
+	
+}
   
 }
