efa9ece6116826953b8125a4a86bc479dda7e4b9
Ebtehal Aljedaani <33575887+bebuxp@users.noreply.github.com>
2017-12-22 00:29:55 +0300

    Update HoldQueue2.java

--- HoldQueue2.java
+++ HoldQueue2.java
@@ -4,6 +4,66 @@ package queue;
 import java.util.LinkedList;
 import objects.Job;
 
+
+/**
+
+ * @author Ebtehal
+
+ */
+
 public class HoldQueue2 extends JobQueue {
+
+     /**
+     * Hold Queue #2 object constructor based on FCFS algorithm.
+     */
+
+    public HoldQueue2 (){
+
+      //creation queue baesd on linkedlist
+       queue =  new LinkedList<Job>();
+
+    }
+  
+  //-------------------------------------------
+  
+  /**
+     * Insert element at rear in Queue by rewrite the method in same class of queue.
+*/
+  
+    @Override
+    public void enqueue(Job job) {
+        queue.addLast(job);
+    }
+//----------------------------------------------
+  
+  /**
+        * Removes elements from front of Queue by rewrite the method in same class of queue.
+     */
+    @Override
+    public Job dequeue() {
+
+       Job head ; //var for set the first job after delete.
+      if(!queue.isEmpty())
+      { 
+            head = (Job) queue.removeFirst();
+       return head ; 
+      }
+	  else 
+		  return null; 
+
+    } 
+
+  //--------------------------------------------------
+  
+   /**
+         * peek method used to retrieve the head of queue without remove it.
+    */
+  
+  @Override
+    public Job peek() {
+
+        return (Job)queue.peek();
+
+    }
 
 }
