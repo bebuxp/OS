5af6f2bc923283792d04efd531c1c5f7079b51a9
Waad <34739949+waadkall@users.noreply.github.com>
2017-12-22 13:38:41 +0300

    Update CompleteQueue.java

--- CompleteQueue.java
+++ CompleteQueue.java
@@ -1,9 +1,32 @@
 package queue;
-
 import java.util.LinkedList;
-import objects.Process;
+import objects.Process; 
 
-public class CompleteQueue extends ProcessQueue{
- 
- 
+/**
+ *
+ * @author Shahad
+ */
+
+public abstract class CompleteQueue extends ProcessQueue {
+        
+    public CompleteQueue(){
+        queue = new LinkedList<Process>();
+    }
+    
+    @Override
+    public void enqueue(Process item){
+        if(!queue.contains((Object)item))     
+            queue.addLast(item); //add the item at the end of the queue 
+    }
+    
+    @Override
+    public Process dequeue(){
+        Process head = (Process)queue.removeFirst(); //remove the item at the the head of the queue 
+        return head;
+    }  
+    
+    @Override
+    public Process peek(){
+    return (Process)queue.peek(); //return the item in the head 
+    }
 }
