5f683c83621758a0a8b2f7e83e6512d31b223bba
Rahaf Albogami <34467652+Rofa0725@users.noreply.github.com>
2017-12-22 12:11:11 +0300

    Update ProcessQueue.java

--- ProcessQueue.java
+++ ProcessQueue.java
@@ -5,15 +5,21 @@ import objects.Process;
 
 public abstract class ProcessQueue extends Queue{
     
+    
+/**
+ *
+ * @author Rahaf Albogami
+ */
+    
     @Override 
-    public abstract void enqueue(Queue item);
+    public abstract void enqueue(Process item);
     
     
     @Override
-    public abstract Queue dequeue();
+    public abstract Process dequeue();
     
     
     @Override
-    public abstract Queue peek();
+    public abstract Process peek();
 
 }
