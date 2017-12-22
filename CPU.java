cb80b9ac0619dfa1a6daad3773c38bbb43996e91
Ebtehal Aljedaani <33575887+bebuxp@users.noreply.github.com>
2017-12-22 15:17:46 +0300

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -3,7 +3,116 @@ package resources;
 
 import objects.Process;
 
-public class CPU {
-   
 
+/**
+ *
+ * @author Ebtehal
+ */
+
+public class CPU
+{
+   // initially create all variables needed in (set & get) methods. based on Output File.
+   
+    private Process Process_run; // object of process.
+    private int Quantum_time;     // compute the time of quantum.
+    private boolean inProcessing;  // check for processing
+    private boolean path;  //check path (is from long queue).
+   
+   
+   /**
+     * CPU object constructor.
+     */
+
+   public CPU() {
+       Process_run = null;
+        inProcessing = false;
+    }
+   
+   //---------------------------------------------------------
+   //METHODS..
+   //---------------------------------------------------------
+   
+   
+   /**
+     * boolean returned: will check if process is start processing or not.
+     */
+   public boolean isProcessing() {
+        return inProcessing;
+    }
+   
+   //----------------------------------------
+   
+   
+   //-----------------
+   //SETING METHODS..
+   //-----------------
+   
+   
+   /**
+     * set the state of process. (true or false).
+     */
+    public void setProcessing(boolean inP) {
+        this.inProcessing = inP;
+    }
+   
+   //-----------------------------------------
+   
+   /**
+     * set the process that will start the running.
+     */
+   
+   public void setProcessRun(Process P_run) {
+        this.Process_run = p_run;
+    }
+   
+//-----------------------------------------------
+  
+   /**
+     * set the quantum time.
+     */
+   
+   public void setQuantumTime(int QTime) {
+        this.Quantum_time = QTime;
+    }
+   
+//--------------------------------------------------  
+
+   
+   /**
+     * set the path of process ( is from the long queue -> "True" , or not -> "False").
+     */
+   
+   public void setProcessPath(boolean path) {
+        this.path = path;
+    }
+   
+//---------------------------------------------------
+   
+   //-----------------
+   //GETTING METHODS..
+   //-----------------
+ 
+   
+    /**
+     * retrun the process it is run.
+     */
+    public Process getProcessRun() {
+        return Process_run;
+    }
+ //-----------------------------------------------------
+   
+    /**
+     * retrun quantum time.
+     */
+   public int getQuantumTime() {
+        return Quantum_time;
+    }
+ //------------------------------------------------------
+   
+    /**
+     * retrun true if process from long queue.
+     */
+   public boolean getProcessPath() {
+        return path;
+    }
 }
