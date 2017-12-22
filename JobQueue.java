da227efa12952e5819a3ccbfd8fac9785a0130d5
Waad <34739949+waadkall@users.noreply.github.com>
2017-12-22 10:57:43 +0300

    Update JobQueue.java

--- JobQueue.java
+++ JobQueue.java
@@ -1,9 +1,19 @@
-
 package queue;
-
 import objects.Job;
 
-public abstract class JobQueue extends Queue{
-    
- 
+/**
+ *
+ * @author Waad
+ */
+
+public abstract class JobQueue extends queue{    
+
+    @Override
+    public abstract void enqueue(Job job);
+
+    @Override
+    public abstract Job dequeue();
+
+    @Override
+    public abstract Job peek(); 
 }
