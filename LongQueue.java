121038e4e3c86efa1e4f2316e88585039ed3a6a3
Waad <34739949+waadkall@users.noreply.github.com>
2017-12-22 14:39:07 +0300

    Update LongQueue.java

--- LongQueue.java
+++ LongQueue.java
@@ -8,23 +8,28 @@ import objects.Process;
  *
  * @author sahar
  */
+
 public class LongQueue extends ProcessQueue{
+    protected int Quantum; //time quantum used in Round Robin Scheduling algorithm
     
      public LongQueue (){
        queue =  new LinkedList<Process>();//creation queue baesd on linkedlist
     }
+    
+    public LongQueue (int quantum){
+        this.Quantum=quantum;
+        queue =  new LinkedList<Process>();//creation queue baesd on linkedlist
+    }
 
     @Override
     public void enqueue(Process newProc) {      
-            queue.addLast(newProc);
-     
+            queue.addLast(newProc); 
     }
 
     @Override
     public Process dequeue() {
         if (!queue.isEmpty())
-            return (Process)queue.removeFirst();
-        
+            return (Process)queue.removeFirst();     
         return null;
     }
 
@@ -37,4 +42,12 @@ public class LongQueue extends ProcessQueue{
     public boolean isQueueEmpty(){
   	return queue.isEmpty();    
    }
+    
+    public void setTimeQuantum(int quantum) {
+        this.Quantum = quantum;
+    }
+
+    public int getQuantum() {
+        return Quantum;
+    } 
 }
