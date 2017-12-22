7916a650ad772353311dccebf44ee942ab93a614
Waad <34739949+waadkall@users.noreply.github.com>
2017-12-22 12:28:32 +0300

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,73 +1,47 @@
-
 package queue;
 
 import java.util.LinkedList;
-import objects.Job;
+
 
 /**
-
  * @author Ebtehal
-
+ *
  */
 
-public abstract class Queue  {
-
-  LinkedList queue queue=new LinkedList<Job>();  // creation of queue based on Linked List
-
+public abstract class queue  { 
+    LinkedList queue; // creation of queue based on Linked List
 	
-//-------------------------	
-//    METHODS OF QUEUE.
-//-------------------------	
-	
-   /**
-      * check if queue have elements
-   */	
-
-  public boolean isQueueEmpty()         
-   {
+    public queue(){}
+    
+//METHODS
+//-------------------------		
+   /*check if queue have elements*/
+  public boolean isQueueEmpty(){
   	return queue.isEmpty();    // (replace by correct call)
    }
   
-
-// ------------------------------
-	
-    /**
-       * Insert element at rear in Queue
-    */
-  public  void enqueue( Job job)
-   {
-        queue.addLast(job);
+//------------------------------
+    /*Insert element at rear in Queue*/
+    public  void enqueue(Object object){
+        queue.addLast(object);
    }
 	
-// -------------------------------
-
-    /**
-        * Removes elements from front of Queue 
-     */
-     public Job dequeue()
-    {
-          if(!queue.isEmpty())
-            return (Job) queue.removeFirst();
-	  else 
-		  return null; 
+//-------------------------------
+    /*Removes elements from front of Queue*/
+     public Object dequeue(){
+        if(!queue.isEmpty())
+            return (Object) queue.removeFirst();
+        else 
+            return null; 
     }
 	
 //------------------------------
-    /**
-         * peek method used to retrieve the head ofqueue without remove it.
-    */
-  
+    /* peek method used to retrieve the head of queue without remove it*/
 	public abstract Object peek();
 	
-//-----------------------------------
-	
-        /**
-	* retrun the size of queue.
-	*/
-	
-       public int get_size()
-       {
-	  return queue.size();
+//------------------------------
+    /*Return the size of queue.*/
+        public int get_size(){
+            return queue.size();
        }
-  	
 }
