7ea12eadddcd26d16017958273f9aefd41b91a48
sahar99 <34767351+sahar99@users.noreply.github.com>
2017-12-22 14:07:14 +0300

    Update LongQueue.java

--- LongQueue.java
+++ LongQueue.java
@@ -4,7 +4,36 @@ package queue;
 import java.util.LinkedList;
 import objects.Process;
 
+/**
+ *
+ * @author sahar
+ */
+public class LongQueue extends ProcessQueue{
+    
+     public LongQueue (){
+       queue =  new LinkedList<Process>();//creation queue baesd on linkedlist
+    }
 
-public class LongQueue extends ProcessQueue {
+    @Override
+    public void enqueue(Process newProc) {      
+            queue.add(newProc);
+     
+    }
 
+    @Override
+    public Process dequeue() {
+        if (!queue.isEmpty())
+            return (Process)queue.remove();
+        
+        return null;
+    }
+
+    @Override
+    public Process peek() {
+        return (Process)queue.peek();   
+    }
+    
+    public boolean isQueueEmpty(){
+  	return queue.isEmpty();    
+   }
 }
